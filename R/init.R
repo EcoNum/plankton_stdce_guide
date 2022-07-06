2cf669f73666f4017dd11291062ab368635743d9
GuyliannEngels <guyliann.engels@umons.ac.be>
2022-07-06 15:18:02 +0200

    advanced classification : plankton_gelatinous

--- R/init.R
+++ R/init.R
@@ -1,9 +1,7 @@
 #
-
 # initialisation ----
 training_dir <- "training_set/_train/_train_classif8/"
 fs::dir_exists(training_dir)
-
 
 # Functions -----
 plot_vignettes <- function(paths, group = NULL, nx = 3, ny = 3, main = NULL, seed = 43, ...) {
